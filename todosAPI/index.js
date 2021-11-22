6d41bf15b71f73c1fb6e8f73e7ddc87ad5f91256
harshit053 <harshitgupta.gh53@gmail.com>
2021-11-22 18:41:25 +0530

    API created

--- todosAPI/index.js
+++ todosAPI/index.js
@@ -1,6 +1,6 @@
 var express = require("express"),
     app     = express(),
-    port    = process.env.post || 3000,
+    port    = process.env.port || 3000,
     bodyParser = require("body-parser");
 
 var todoRoutes = require('./routes/todos');
