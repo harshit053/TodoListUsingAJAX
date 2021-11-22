d0fa405f5b1a7ade66be4267113ab94883f3a3fa
harshit053 <harshitgupta.gh53@gmail.com>
2021-11-22 18:39:55 +0530

    API created

---
+++ todosAPI/index.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+    app     = express(),
+    port    = process.env.post || 3000,
+    bodyParser = require("body-parser");
+
+var todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+    res.send("From root route");
+})
+
+app.use("/api/todo", todoRoutes);
+
+app.listen(port, function() {
+    console.log("App running on port " + port);
+})
---
+++ todosAPI/routes/todos.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    router  = express.Router(),
+    db      = require("../models"),
+    helpers = require("../helpers/todos");
+
+router.route('/')
+    .get(helpers.getTodos)
+    .post(helpers.createTodos)
+
+router.route('/:todoId')
+    .get(helpers.showTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo)
+
+module.exports = router;
